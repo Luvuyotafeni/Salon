ff552efcd3df495d40cb4060e06f5399788e9c0b
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-13 16:59:54 +0200

    project reinitialized

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
     <div>
-        
+      <header>
+        <div>
+          <div>
+
+          </div>
+          <div>
+            <ul>
+              <li><a href=''>Home</a></li>
+              <li><a href=''>About</a></li>
+              <li><a href=''>Services</a></li>
+              <li><a href=''>Gallery</a></li>
+              <li><a href=''>Products</a></li>
+              <li><a href=''>Contact Us</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </header>
     </div>
   )
 }
