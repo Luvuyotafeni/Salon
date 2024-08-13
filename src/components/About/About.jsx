92cfe6f34149f87e952b273543e7034d9feda3de
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-13 19:23:17 +0200

    fixed the about component margins

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,15 +7,17 @@ const About = () => {
     <>
         <div>
             <h2 className='text-center'>About</h2>
-            <div className='about'>
-                <img src={aboutimg}/>
-            </div>
-            <div>
-                <p> Welcome to Barbie Hair, the premium hair and nail salon in Durban, proudly owned by Sphe Sithole.
-                    At Barbie Hair, we specialize in providing top-notch hair and nail services that leave our clients
-                    feeling beautiful and confident. Whether you're looking for a fresh and stunning new hairstyle,
-                    or perfectly manicured nails, our talented and experienced team is here to bring your beauty vision
-                    to life. Join us at Barbie Hair and experience the ultimate in salon luxury and personalized care. </p>
+            <div className='about-info'>
+                <div className='about'>
+                    <img src={aboutimg}/>
+                </div>
+                <div>
+                    <p> Welcome to Barbie Hair, the premium hair and nail salon in Durban, proudly owned by Sphe Sithole.
+                        At Barbie Hair, we specialize in providing top-notch hair and nail services that leave our clients
+                        feeling beautiful and confident. Whether you're looking for a fresh and stunning new hairstyle,
+                        or perfectly manicured nails, our talented and experienced team is here to bring your beauty vision
+                        to life. Join us at Barbie Hair and experience the ultimate in salon luxury and personalized care. </p>
+                </div>
             </div>
         </div>
     </>
