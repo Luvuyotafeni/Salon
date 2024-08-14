9ef30a1253dd49437c04b0568cf7f2f0df53e880
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-15 00:23:24 +0200

    carousel successfully shows and slides

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,44 +1,48 @@
-import React from 'react'
-import Manicure from '../Assets/Manicure.jpg'
-import Make_up from '../Assets/Make Up.jpeg'
-import Hair from '../Assets/Hair.jpeg'
+import React from 'react';
+import Manicure from '../Assets/Manicure.jpg';
+import Make_up from '../Assets/Make Up.jpeg';
+import Hair from '../Assets/Hair.jpeg';
+import './Services.css';
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Bootstrap JavaScript
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Services = () => {
   return (
     <div>
         <p>Services</p>
-        <div>
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img class="d-block w-100" src={Manicure} alt="First slide"></img>
+        <div className='carousel'>
+            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"></li>
+                    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+                    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src={Manicure} alt="First slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={Make_up} alt="Second slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={Hair} alt="Third slide" />
+                    </div>
                 </div>
-                <div class="carousel-item">
-                <img class="d-block w-100" src={Hair} alt="Second slide"></img>
-                </div>
-                <div class="carousel-item">
-                <img class="d-block w-100" src={Make_up} alt="Third slide"></img>
-                </div>
+                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-            </div>
-
         </div>
-
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
