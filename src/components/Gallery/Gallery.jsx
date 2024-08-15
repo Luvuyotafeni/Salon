a7e33d6b68dcdc07581a9def320523a420c9e02b
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-15 21:19:30 +0200

    Successful gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -10,23 +10,23 @@ const Gallery = () => {
     <div className="container">
       <h2 className='center'>Gallery</h2>
       <div className="row">
-        <div className="col-md-4">
-          <div className="card middle-card">
+        <div className="col-md-4 side">
+          <div className="card middle-card side">
             <img src={G1} className="card-img-top" alt="Side Image 1" />
    
           </div>
         </div>
 
-        <div className="col-md-4">
+        <div className="col-md-4 side">
           <div className="card ">
             <img src={G2}className="card-img-top" alt="Middle Image" />
             
           </div>
         </div>
 
-        <div className="col-md-4">
-          <div className="card middle-card">
-            <img src={G3} className="card-img-top" alt="Side Image 2" />
+        <div className="col-md-4 side">
+          <div className="card middle-card side">
+            <img src={G3} className="card-img-top " alt="Side Image 2" />
           </div>
         </div>
       </div>
