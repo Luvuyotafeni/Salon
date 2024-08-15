2febdfc9f9903f4a91c5c8220a4efbca8d4bbcf2
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-15 20:25:19 +0200

    gallery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About/About';
+import Gallery from './components/Gallery/Gallery';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Services from './components/Services/Services';
@@ -11,6 +12,7 @@ function App() {
       <Home/>
       <About/>
       <Services/>
+      <Gallery/>
     </>
   );
 }
