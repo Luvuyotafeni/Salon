1295fbe203abb2a17d9a7c3aa03d5efa06129111
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-15 20:48:53 +0200

    section without description

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,34 +4,25 @@ import './Gallery.css'; // Import your custom styles
 const Gallery = () => {
   return (
     <div className="container">
+      <h2 className='center'>Gallery</h2>
       <div className="row">
         <div className="col-md-4">
           <div className="card">
             <img src="https://via.placeholder.com/350x150" className="card-img-top" alt="Side Image 1" />
-            <div className="card-body">
-              <h5 className="card-title">Image 1</h5>
-              <p className="card-text">Description of the first image.</p>
-            </div>
+   
           </div>
         </div>
 
         <div className="col-md-4">
           <div className="card middle-card">
             <img src="https://via.placeholder.com/350x300" className="card-img-top" alt="Middle Image" />
-            <div className="card-body">
-              <h5 className="card-title">Image 2</h5>
-              <p className="card-text">Description of the middle image.</p>
-            </div>
+            
           </div>
         </div>
 
         <div className="col-md-4">
           <div className="card">
             <img src="https://via.placeholder.com/350x150" className="card-img-top" alt="Side Image 2" />
-            <div className="card-body">
-              <h5 className="card-title">Image 3</h5>
-              <p className="card-text">Description of the third image.</p>
-            </div>
           </div>
         </div>
       </div>
