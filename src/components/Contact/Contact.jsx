f604decf996803deccaa030a350e4b05b0fa38c6
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-17 22:11:56 +0200

    contact compoments sucessfully added, with no borders, about to add the working hours

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,29 +8,26 @@ const Contact = () => {
         <div className='container'>
           <div className="row">
           <div className="col-md-4">
-            <div className="card middle-card">
+            <div className="card-body">
               <img src={log} className="card-img-top" alt="Side Image 1" />
+              
+            </div>
+          </div>
+
+          <div className="col-md-4">
+ 
               <div className="card-body">
                 <h5 className="card-title">Image 1</h5>
                 <p className="card-text">Description of the first image.</p>
-              </div>
-            </div>
+              </div>             
+
           </div>
 
           <div className="col-md-4">
-            <div className="card middle-card">
-              <img src={Nails2} className="card-img-top" alt="Middle Image" />
-              <div className="card-body">
-                <h5 className="card-title">Image 2</h5>
-                <p className="card-text">Description of the middle image.</p>
+          <div className="card-body">
+                <h5 className="card-title">Image 1</h5>
+                <p className="card-text">Description of the first image.</p>
               </div>
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card middle-card">
-             
-            </div>
           </div>
         </div>
       </div>
