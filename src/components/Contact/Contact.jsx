8c4d458e8c2ae85d9c7a2c4a2b20c3c8f8a5ad1b
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-22 08:40:54 +0200

    reservation modal initialization

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -37,7 +37,14 @@ const Contact = () => {
                   </div>
               </div>
             </div>
-            <button className='btn btn-dark'>Make Reservation</button>
+            <button type="button" className="btn btn-dark" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+                <div className="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                  <div className="modal-dialog modal-lg">
+                    <div className="modal-content">
+                      ...
+                    </div>
+                  </div>
+                </div>
           </div>
 
         </div>
