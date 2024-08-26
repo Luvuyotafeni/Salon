e7d921dd4d65709e07c218c1996f9353bbb419ea
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-26 11:07:11 +0200

    added the media queries for the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,28 +9,29 @@ const Header = () => {
   };
 
   return (
-    <div>
-      <header className='header'>
-        <div className='container d-flex'>
-          <div className='logo'>
-            {/* Add your logo here */}
-          </div>
-          <div className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
-            <ul>
-              <li><a href='#'>Home</a></li>
-              <li><a href='#'>About</a></li>
-              <li><a href='#'>Services</a></li>
-              <li><a href='#'>Gallery</a></li>
-              <li><a href='#'>Products</a></li>
-              <li><a href='#'>Contact Us</a></li>
-            </ul>
-          </div>
-          <div className='hamburger' onClick={toggleMenu}>
-            <i className={`bx ${isMenuOpen ? 'bx-x' : 'bx-menu'}`}></i>
-          </div>
+    <header className='header'>
+      <div className='container d-flex'>
+        <div className='logo'>
+          {/* Add your logo here */}
         </div>
-      </header>
-    </div>
+        <div className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
+          <div className='close-icon' onClick={toggleMenu}>
+            <i className='bx bx-x'></i>
+          </div>
+          <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Services</a></li>
+            <li><a href='#'>Gallery</a></li>
+            <li><a href='#'>Products</a></li>
+            <li><a href='#'>Contact Us</a></li>
+          </ul>
+        </div>
+        <div className='hamburger' onClick={toggleMenu}>
+          <i className={`bx ${isMenuOpen ? 'bx-x' : 'bx-menu'}`}></i>
+        </div>
+      </div>
+    </header>
   );
 }
 
