9a67a21b4927e83f4a130ea724a8a370ddc8b050
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-08-26 09:58:59 +0200

    adding the media queries for the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import './Header.css'
+import React, { useState } from 'react';
+import './Header.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
       <header className='header'>
         <div className='container d-flex'>
-          <div className='logo '>
-
+          <div className='logo'>
+            {/* Add your logo here */}
           </div>
-          <div >
+          <div className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
             <ul>
-              <li><a href=''>Home</a></li>
-              <li><a href=''>About</a></li>
-              <li><a href=''>Services</a></li>
-              <li><a href=''>Gallery</a></li>
-              <li><a href=''>Products</a></li>
-              <li><a href=''>Contact Us</a></li>
+              <li><a href='#'>Home</a></li>
+              <li><a href='#'>About</a></li>
+              <li><a href='#'>Services</a></li>
+              <li><a href='#'>Gallery</a></li>
+              <li><a href='#'>Products</a></li>
+              <li><a href='#'>Contact Us</a></li>
             </ul>
           </div>
-
+          <div className='hamburger' onClick={toggleMenu}>
+            <i className={`bx ${isMenuOpen ? 'bx-x' : 'bx-menu'}`}></i>
+          </div>
         </div>
       </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
